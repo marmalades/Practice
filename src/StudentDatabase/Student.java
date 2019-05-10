d51ab319d9d3baf85ec181e1a40f3a7fd6720c68
Mario <codingphs@gmail.com>
2019-05-10 18:35:55 -0500

    init StudentDatabase package

---
+++ src/StudentDatabase/Student.java
@@ -0,0 +1,25 @@
+package StudentDatabase;
+
+public class Student
+{
+    private String firstName;
+    private String lastName;
+    private String classification;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance; //each course is 600. maybe add credit hours in DB
+    private int costOfCourse = 600;
+
+
+    //Constructor: prompt user to enter student name and year
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View balance
+
+    //Pay tuition
+
+    //Show status
+}
---
+++ src/StudentDatabase/StudentDatabaseApp.java
@@ -0,0 +1,4 @@
+package StudentDatabase;
+
+public class StudentDatabaseApp {
+}
