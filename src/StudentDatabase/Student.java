445b7863c0e4295840f4e5ffa263f0f6cf036178
Mario <codingphs@gmail.com>
2019-05-10 18:50:18 -0500

    Basic constructors setup

--- src/StudentDatabase/Student.java
+++ src/StudentDatabase/Student.java
@@ -1,10 +1,12 @@
 package StudentDatabase;
 
+import java.util.Scanner;
+
 public class Student
 {
     private String firstName;
     private String lastName;
-    private String classification;
+    private int classification;
     private int studentID;
     private String courses;
     private int tuitionBalance; //each course is 600. maybe add credit hours in DB
@@ -12,6 +14,22 @@ public class Student
 
 
     //Constructor: prompt user to enter student name and year
+    public Student ()
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshman \n2 - Sophomore \n3 - Junior \n4 - Senior \nEnter student classification: ");
+        this.classification = in.nextInt();
+
+        System.out.println();
+
+    }
 
     //Generate an ID
 
--- src/StudentDatabase/StudentDatabaseApp.java
+++ src/StudentDatabase/StudentDatabaseApp.java
@@ -1,4 +1,13 @@
 package StudentDatabase;
 
-public class StudentDatabaseApp {
+public class StudentDatabaseApp
+{
+    public static void main(String[] args)
+    {
+        Student student1 = new Student();
+    }
+
+    //ask how many users we want
+
+    //create n number of users
 }
