8627d981258f82066b21a229e32aa11ef9c7e416
Mario <codingphs@gmail.com>
2019-05-10 18:59:06 -0500

    Added student ID generator

--- src/StudentDatabase/Student.java
+++ src/StudentDatabase/Student.java
@@ -7,11 +7,11 @@ public class Student
     private String firstName;
     private String lastName;
     private int classification;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance; //each course is 600. maybe add credit hours in DB
-    private int costOfCourse = 600;
-
+    private static int costOfCourse = 600;
+    private static int ID = 100;
 
     //Constructor: prompt user to enter student name and year
     public Student ()
@@ -27,11 +27,18 @@ public class Student
         System.out.println("1 - Freshman \n2 - Sophomore \n3 - Junior \n4 - Senior \nEnter student classification: ");
         this.classification = in.nextInt();
 
-        System.out.println();
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + ": " + classification + " " + studentID);
 
     }
 
     //Generate an ID
+    private void setStudentID()
+    {//grade level + ID
+        ID++; //increments ID after each user created
+        this.studentID = classification + "" + ID; //wont add numbers but will combine Strings thanks to the "" in the concat process
+    }
 
     //Enroll in courses
 
