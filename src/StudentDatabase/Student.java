614489d1869b4424a840934b05f06644b913b6a6
Mario <codingphs@gmail.com>
2019-05-10 19:19:27 -0500

    added payment

--- src/StudentDatabase/Student.java
+++ src/StudentDatabase/Student.java
@@ -8,13 +8,13 @@ public class Student
     private String lastName;
     private int classification;
     private String studentID;
-    private String courses;
-    private int tuitionBalance; //each course is 600. maybe add credit hours in DB
+    private String courses = null;
+    private int tuitionBalance = 0; //each course is 600. maybe add credit hours in DB
     private static int costOfCourse = 600;
     private static int ID = 100;
 
     //Constructor: prompt user to enter student name and year
-    public Student ()
+    public Student()
     {
         Scanner in = new Scanner(System.in);
 
@@ -41,10 +41,40 @@ public class Student
     }
 
     //Enroll in courses
+    public void enroll()
+    {
+        //get inside a loop, user hits 0 when done enrolling
+
+        do{
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner input = new Scanner(System.in);
+            String course = input.nextLine();
+            if (!course.equals("Q") && !course.equals("q")) //upper or lower
+            {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else {break;}
+        }while (1 != 0);
+
+        System.out.println("ENROLLED IN: " + courses);
+    }
 
     //View balance
-
+    public void viewBalance()
+    {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
     //Pay tuition
+    public void payTuition ()
+    {
+        System.out.print("Enter your payment amount: ");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
 
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Your payment of $" + payment + " has been received.");
+        viewBalance();
+    }
     //Show status
 }
--- src/StudentDatabase/StudentDatabaseApp.java
+++ src/StudentDatabase/StudentDatabaseApp.java
@@ -5,6 +5,9 @@ public class StudentDatabaseApp
     public static void main(String[] args)
     {
         Student student1 = new Student();
+        student1.enroll();
+        student1.payTuition();
+
     }
 
     //ask how many users we want
